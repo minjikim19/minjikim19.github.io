cc88bc4c8bc8839f207f01e66f1778c6b8b4b174
Minji Kim <minjikm19@gmail.com>
2026-02-06 15:09:31 -0500

    Setup publish actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/minjikim19.github.io",
+  assetPrefix: "/minjikim19.github.io/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
