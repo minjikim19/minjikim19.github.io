73e941d6d1eeb33663de5f01c76a07ee3ccc4d06
Minji Kim <minjikm19@gmail.com>
2026-02-06 15:13:45 -0500

    Fix GitHub Pages basePath for user site

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "/minjikim19.github.io",
-  assetPrefix: "/minjikim19.github.io/",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
